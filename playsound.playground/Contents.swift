e8b8095c0b8b6fb5bc115cd93c7d262bac1e8240
fanny <fc2@cin.ufpe.br>
2018-03-28 01:44:10 -0300

    Structing the basic game project

--- playsound.playground/Contents.swift
+++ playsound.playground/Contents.swift
@@ -4,20 +4,30 @@ import UIKit
 import AVFoundation
 import PlaygroundSupport
 
-class MyViewController : UIViewController {
+class MyViewController : UIViewController, AVAudioPlayerDelegate {
     
     let squareView = UIView()
 
     var sounds = [AVAudioPlayer]()
     let soundsNames = ["sound_pink", "sound_yellow", "sound_green", "sound_purple"]
     
+    //colored buttons
     let pinkBtn = UIButton()
     let yellowBtn = UIButton()
     let greenBtn = UIButton()
     let purpleBtn = UIButton()
     
+    let startBtn = UIButton()
+     let label = UILabel()
+    
+    
+    //save the sound sequence of the game
     var soundsSequence = [Int]()
+
+    var numberOfButtonsPressed = 0
     var currentItem = 0
+    var currentLevel = 1
+    let defaultMaxLevel = 5
 
     var gameState = GameState.NotPlaying
     
@@ -26,32 +36,36 @@ class MyViewController : UIViewController {
         let view = UIView()
         view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
        
+
+        self.label.frame = CGRect(x: 150, y: 20, width: 200, height: 20)
+        self.label.text = "Play with me!"
+        self.label.textColor = .black
+        view.addSubview(self.label)
         
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 20, width: 200, height: 20)
-        label.text = "Play with me!"
-        label.textColor = .black
         
-        view.addSubview(label)
+        self.startBtn.frame = CGRect(x: 150, y: 40, width: 100, height: 20)
+        self.startBtn.setTitle("Start Game", for: .normal)
+        self.startBtn.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        self.startBtn.addTarget(self, action: #selector(startGame), for: .touchUpInside)
+        view.addSubview(self.startBtn)
         
-      
-       // squareView.backgroundColor = #colorLiteral(red: 0.6078431373, green: 0.6078431373, blue: 0.6078431373, alpha: 0.2295341258)
-        squareView.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
-        view.addSubview(squareView)
+        
+        self.squareView.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
+        view.addSubview(self.squareView)
         
         self.setupAudioFiles()
         self.setupButtons()
-        startGame()
-        
         self.view = view
     }
     
     @objc func buttonAction(sender: UIButton) {
+        
         sounds[sender.tag].play()
-        print(sender.tag)
+        print("buttonPressed: ", sender.tag)
+        checkPressedButton(buttonPressed: sender.tag)
     }
     
-    
+    //Colored buttons set up
     func setupButtons() {
         
         //Pink button
@@ -87,6 +101,7 @@ class MyViewController : UIViewController {
         squareView.addSubview(purpleBtn)
     }
     
+    //Audio Files set up
     func setupAudioFiles() {
         for sound in soundsNames {
             let soundURL = Bundle.main.url(forResource: sound, withExtension: "wav")!
@@ -96,27 +111,14 @@ class MyViewController : UIViewController {
                  print(error)
             }
         }
-    }
-    
-    func audioPlayerDidFinishPlaying(audioPlayer: AVAudioPlayer, successfully flag: Bool){
-        if currentItem <= soundsSequence.count - 1 {
-            //play the next item
-            playNextRandomSound()
-            print("play next sound")
-        } else {
-            gameState = GameState.UserPlaying
-            resetButtonsHighlights()
-            print("reseting highlight")
+        
+        for audioPlayer in sounds {
+            audioPlayer.delegate = self
+            audioPlayer.numberOfLoops = 0
         }
     }
     
-    func playNextRandomSound() {
-        let selectedItem = soundsSequence[currentItem]
-        
-        highlightButton(buttonTag: selectedItem)
-        sounds[selectedItem].play()
-    }
-    
+
     func highlightButton(buttonTag: Int) {
         switch buttonTag {
         case 0:
@@ -141,7 +143,8 @@ class MyViewController : UIViewController {
     }
     
     
-    func resetButtonsHighlights(){
+    func resetButtonsHighlights() {
+
         pinkBtn.setImage(UIImage(named: "pink_btn"), for: .normal)
         yellowBtn.setImage(UIImage(named: "yellow_btn"), for: .normal)
         greenBtn.setImage(UIImage(named: "green_btn"), for: .normal)
@@ -149,16 +152,88 @@ class MyViewController : UIViewController {
     }
     
     
-    func startGame() {
+    func addSoundToSequence() {
         let randomNumber = Int(arc4random_uniform(4))
-        print("randomNumber", randomNumber)
         soundsSequence.append(randomNumber)
-        playNextRandomSound()
+    }
+    
+    @objc func startGame() {
+        soundsSequence = []
+        
+        currentLevel = 1
+        print("New level: \(currentLevel)")
+        
+        addSoundToSequence()
+        
+        playNextItem()
+    }
+    
+    func startNewLevel() {
+        currentLevel += 1
+        print("New level: \(currentLevel)")
+        currentItem = 0
+        numberOfButtonsPressed = 0
+        
+        addSoundToSequence()
+        print("Sound Sequence:",soundsSequence)
+        
+        playNextItem()
+    }
+    
+    func playNextItem () {
+        print("played sound :", soundsSequence[currentItem])
+        
+        let playedSound = soundsSequence[currentItem]
+        
+        resetButtonsHighlights()
+        highlightButton(buttonTag: playedSound)
+        sounds[playedSound].play()
+        currentItem += 1
         
     }
     
-
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("Player finish playing", player)
+        
+        if currentItem <= soundsSequence.count - 1 {
+            //adding delay between the sounds
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(50)) {
+                self.playNextItem()
+            }
+            
+        }else{
+            //TO DO: User turn
+            self.resetButtonsHighlights()
+        }
+    }
+    
+    func checkPressedButton(buttonPressed: Int) {
+        
+        if buttonPressed == soundsSequence[numberOfButtonsPressed] {
+            
+            if numberOfButtonsPressed == (defaultMaxLevel - 1) {
+                print("WON THE GAME")
+            } else if numberOfButtonsPressed == soundsSequence.count - 1 {
+                //at the last item of the sequence
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+                //delay for the next level... add animation?
+                    self.startNewLevel()
+                }
+            }
+            numberOfButtonsPressed += 1
+            
+        } else {
+            //GAME OVER
+            print("game over!")
+            label.text = "GAME OVER!"
+            
+        }
+    }
 }
+
+
+
 // Present the view controller in the Live View window
 let viewController = MyViewController()
 viewController.preferredContentSize = CGSize(width: 400, height: 400)
