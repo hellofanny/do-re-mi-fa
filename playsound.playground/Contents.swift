8c05a06303f0f621cd7808736b284d5c86184ab0
fanny <fc2@cin.ufpe.br>
2018-03-29 15:33:18 -0300

    Changing between gamemode and freestyle

--- playsound.playground/Contents.swift
+++ playsound.playground/Contents.swift
@@ -1,5 +1,5 @@
 //: A UIKit based Playground for presenting user interface
-  
+
 import UIKit
 import AVFoundation
 import PlaygroundSupport
@@ -12,7 +12,7 @@ class MyViewController : UIViewController, AVAudioPlayerDelegate {
     let squareView = UIView()
     
     var doremifaMode : Mode = Mode.GameMode
-
+    
     var sounds = [AVAudioPlayer]()
     let soundsNames = ["sound_pink", "sound_yellow", "sound_green", "sound_purple"]
     
@@ -22,35 +22,26 @@ class MyViewController : UIViewController, AVAudioPlayerDelegate {
     let greenBtn = UIButton()
     let purpleBtn = UIButton()
     
-    let startGameBtn = UIButton()
+    let playBtn = UIButton()
     let label = UILabel()
     let modeBtn = UIButton()
     
+    var game : DoReMiFaGame!
     
-    //save the sound sequence of the game
-    var soundsSequence = [Int]()
-
-    var numberOfButtonsPressed = 0
-    var currentItem = 0
-    var currentLevel = 1
-    let defaultMaxLevel = 5
-
-    var gameState = GameState.NotPlaying
     
     override func loadView() {
         
         let view = UIView()
         view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-       
+        
         let logoView = UIImageView(image: UIImage(named: "doremifa_logo"))
         logoView.frame = CGRect(x: 150, y: 20, width: 180, height: 77)
         view.addSubview(logoView)
         
-       
-modeBtn.setImage(UIImage(named: "gameMode_btn"), for: .normal)
-        modeBtn.setImage(UIImage(named: "freestyle_btn"), for: .highlighted )
-         self.modeBtn.frame = CGRect(x: 170, y: logoView.frame.origin.y + 85 , width: 152, height: 25)
-         self.modeBtn.addTarget(self, action: #selector(changeMode), for: .touchUpInside)
+        self.modeBtn.setImage(UIImage(named: "gameMode_btn"), for: .normal)
+        self.modeBtn.setImage(UIImage(named: "freestyle_btn"), for: .highlighted )
+        self.modeBtn.frame = CGRect(x: 170, y: logoView.frame.origin.y + 85 , width: 152, height: 25)
+        self.modeBtn.addTarget(self, action: #selector(self.changeMode), for: .touchUpInside)
         view.addSubview(modeBtn)
         
         self.label.frame = CGRect(x: 200, y: 400, width: 200, height: 20)
@@ -59,42 +50,42 @@ modeBtn.setImage(UIImage(named: "gameMode_btn"), for: .normal)
         self.label.text = "Play with me!"
         view.addSubview(self.label)
         
-        
         self.squareView.frame = CGRect(x: 150, y: 160, width: 200, height: 200)
         view.addSubview(self.squareView)
         
-        self.startGameBtn.setImage(UIImage(named: "play_btn"), for: .normal)
-        self.startGameBtn.frame = CGRect(x: (squareView.frame.width)/2 - 30, y: (squareView.frame.height/2) - 30, width: 60, height: 60)
-        self.startGameBtn.addTarget(self, action: #selector(startGame), for: .touchUpInside)
-        squareView.addSubview(self.startGameBtn)
+        self.playBtn.setImage(UIImage(named: "play_btn"), for: .normal)
+        self.playBtn.frame = CGRect(x: (squareView.frame.width)/2 - 30, y: (squareView.frame.height/2) - 30, width: 60, height: 60)
+        self.playBtn.addTarget(self, action: #selector(self.startGame), for: .touchUpInside)
+        squareView.addSubview(self.playBtn)
         
         self.setupAudioFiles()
         self.setupButtons()
+        self.game = DoReMiFaGame(sounds: sounds, pinkBtn: self.pinkBtn, yellowBtn: self.yellowBtn, greenBtn: self.greenBtn, purpleBtn: self.purpleBtn)
+        
         self.view = view
     }
     
     @objc func buttonAction(sender: UIButton) {
-        
         if doremifaMode == Mode.Freestyle {
             sounds[sender.tag].play()
         } else {
             sounds[sender.tag].play()
             print("buttonPressed: ", sender.tag)
-            checkPressedButton(buttonPressed: sender.tag)
+            self.game.checkPressedButton(buttonPressed: sender.tag)
         }
     }
     
     @objc func changeMode() {
-        if doremifaMode == Mode.GameMode {
-            doremifaMode = Mode.Freestyle
-             self.startGameBtn.isHidden = true
-            modeBtn.setImage(UIImage(named: "freestyle_btn"), for: .normal)
-            modeBtn.setImage(UIImage(named: "gameMode_btn"), for: .highlighted )
+        if self.doremifaMode == Mode.GameMode {
+            self.doremifaMode = Mode.Freestyle
+            self.playBtn.isHidden = true
+            self.modeBtn.setImage(UIImage(named: "freestyle_btn"), for: .normal)
+            self.modeBtn.setImage(UIImage(named: "gameMode_btn"), for: .highlighted)
         } else {
-            doremifaMode = Mode.GameMode
-            self.startGameBtn.isHidden = false
-            modeBtn.setImage(UIImage(named: "gameMode_btn"), for: .normal)
-            modeBtn.setImage(UIImage(named: "freestyle_btn"), for: .highlighted )
+            self.doremifaMode = Mode.GameMode
+            self.playBtn.isHidden = false
+            self.modeBtn.setImage(UIImage(named: "gameMode_btn"), for: .normal)
+            self.modeBtn.setImage(UIImage(named: "freestyle_btn"), for: .highlighted )
         }
     }
     
@@ -103,36 +94,36 @@ modeBtn.setImage(UIImage(named: "gameMode_btn"), for: .normal)
     func setupButtons() {
         
         //Pink button
-        pinkBtn.setImage(UIImage(named: "pink_btn"), for: .normal)
-        pinkBtn.setImage(UIImage(named: "pink_btn_on"), for: .highlighted)
-        pinkBtn.frame = CGRect(x: (squareView.frame.width)/2 - 30, y: 0, width: 60, height: 60)
-        pinkBtn.tag = 0
-        pinkBtn.addTarget(self, action: #selector(buttonAction(sender:)), for: .touchUpInside)
-        squareView.addSubview(pinkBtn)
+        self.pinkBtn.setImage(UIImage(named: "pink_btn"), for: .normal)
+        self.pinkBtn.setImage(UIImage(named: "pink_btn_on"), for: .highlighted)
+        self.pinkBtn.frame = CGRect(x: (squareView.frame.width)/2 - 30, y: 0, width: 60, height: 60)
+        self.pinkBtn.tag = 0
+        self.pinkBtn.addTarget(self, action: #selector(buttonAction(sender:)), for: .touchUpInside)
+        self.squareView.addSubview(pinkBtn)
         
         //Yellow button
-        yellowBtn.setImage(UIImage(named: "yellow_btn"), for: .normal)
-        yellowBtn.setImage(UIImage(named: "yellow_btn_on"), for: .highlighted)
-        yellowBtn.frame = CGRect(x: (squareView.frame.width)/2 - 30, y: squareView.frame.height - 60 , width: 60, height: 60)
-        yellowBtn.tag = 1
-        yellowBtn.addTarget(self, action: #selector(buttonAction(sender:)), for: .touchUpInside)
-        squareView.addSubview(yellowBtn)
+        self.yellowBtn.setImage(UIImage(named: "yellow_btn"), for: .normal)
+        self.yellowBtn.setImage(UIImage(named: "yellow_btn_on"), for: .highlighted)
+        self.yellowBtn.frame = CGRect(x: (squareView.frame.width)/2 - 30, y: squareView.frame.height - 60 , width: 60, height: 60)
+        self.yellowBtn.tag = 1
+        self.yellowBtn.addTarget(self, action: #selector(buttonAction(sender:)), for: .touchUpInside)
+        self.squareView.addSubview(yellowBtn)
         
         //Green button
-        greenBtn.setImage(UIImage(named: "green_btn"), for: .normal)
-        greenBtn.setImage(UIImage(named: "green_btn_on"), for: .highlighted)
-        greenBtn.frame = CGRect(x: 0, y: (squareView.frame.height/2) - 30 , width: 60, height: 60)
-        greenBtn.tag = 2
-        greenBtn.addTarget(self, action: #selector(buttonAction(sender:)), for: .touchUpInside)
-        squareView.addSubview(greenBtn)
+        self.greenBtn.setImage(UIImage(named: "green_btn"), for: .normal)
+        self.greenBtn.setImage(UIImage(named: "green_btn_on"), for: .highlighted)
+        self.greenBtn.frame = CGRect(x: 0, y: (squareView.frame.height/2) - 30 , width: 60, height: 60)
+        self.greenBtn.tag = 2
+        self.greenBtn.addTarget(self, action: #selector(buttonAction(sender:)), for: .touchUpInside)
+        self.squareView.addSubview(greenBtn)
         
         //Purple button
-        purpleBtn.setImage(UIImage(named: "purple_btn"), for: .normal)
-        purpleBtn.setImage(UIImage(named: "purple_btn_on"), for: .highlighted)
-        purpleBtn.frame = CGRect(x: (squareView.frame.width)/2 + 40 , y: (squareView.frame.height/2) - 30 , width: 60, height: 60)
-        purpleBtn.tag = 3
-        purpleBtn.addTarget(self, action: #selector(buttonAction(sender:)), for: .touchUpInside)
-        squareView.addSubview(purpleBtn)
+        self.purpleBtn.setImage(UIImage(named: "purple_btn"), for: .normal)
+        self.purpleBtn.setImage(UIImage(named: "purple_btn_on"), for: .highlighted)
+        self.purpleBtn.frame = CGRect(x: (squareView.frame.width)/2 + 40 , y: (squareView.frame.height/2) - 30 , width: 60, height: 60)
+        self.purpleBtn.tag = 3
+        self.purpleBtn.addTarget(self, action: #selector(buttonAction(sender:)), for: .touchUpInside)
+        self.squareView.addSubview(purpleBtn)
     }
     
     //Audio Files set up
@@ -140,131 +131,32 @@ modeBtn.setImage(UIImage(named: "gameMode_btn"), for: .normal)
         for sound in soundsNames {
             let soundURL = Bundle.main.url(forResource: sound, withExtension: "wav")!
             do {
-                try sounds.append(AVAudioPlayer(contentsOf: soundURL))
+                try self.sounds.append(AVAudioPlayer(contentsOf: soundURL))
             } catch {
-                 print(error)
+                print(error)
             }
         }
         
-        for audioPlayer in sounds {
+        for audioPlayer in self.sounds {
             audioPlayer.delegate = self
             audioPlayer.numberOfLoops = 0
         }
     }
     
-
-    func highlightButton(buttonTag: Int) {
-        switch buttonTag {
-        case 0:
-            resetButtonsHighlights()
-            pinkBtn.setImage(UIImage(named:"pink_btn_on"), for: .normal)
-            break
-        case 1:
-            resetButtonsHighlights()
-            yellowBtn.setImage(UIImage(named:"yellow_btn_on"), for: .normal)
-            break
-        case 2:
-            resetButtonsHighlights()
-            greenBtn.setImage(UIImage(named:"green_btn_on"), for: .normal)
-            break
-        case 3:
-            resetButtonsHighlights()
-            purpleBtn.setImage(UIImage(named:"purple_btn_on"), for: .normal)
-            break
-        default:
-            break
-        }
-    }
-    
-    
-    func resetButtonsHighlights() {
-
-        pinkBtn.setImage(UIImage(named: "pink_btn"), for: .normal)
-        yellowBtn.setImage(UIImage(named: "yellow_btn"), for: .normal)
-        greenBtn.setImage(UIImage(named: "green_btn"), for: .normal)
-        purpleBtn.setImage(UIImage(named: "purple_btn"), for: .normal)
-    }
-    
-    
-    func addSoundToSequence() {
-        let randomNumber = Int(arc4random_uniform(4))
-        soundsSequence.append(randomNumber)
-    }
     
     @objc func startGame() {
-        soundsSequence = []
-        currentLevel = 1
-        print("New level: \(currentLevel)")
         
-        addSoundToSequence()
-        
-        self.startGameBtn.isHidden = true
+        self.game.resetGame()
+        self.playBtn.isHidden = true
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
-            self.playNextItem()
+            self.game.startNewLevel()
+            print(self.game.currentLevel)
         }
     }
-    
-    func startNewLevel() {
-        currentLevel += 1
-        print("New level: \(currentLevel)")
-        currentItem = 0
-        numberOfButtonsPressed = 0
-        
-        addSoundToSequence()
-        print("Sound Sequence:",soundsSequence)
-        
-        playNextItem()
-    }
-    
-    func playNextItem () {
-        print("played sound :", soundsSequence[currentItem])
-        
-        let playedSound = soundsSequence[currentItem]
-        
-        resetButtonsHighlights()
-        highlightButton(buttonTag: playedSound)
-        sounds[playedSound].play()
-        currentItem += 1
-        
-    }
-    
     
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        //print("Player finish playing", player)
-        
-        if currentItem <= soundsSequence.count - 1 {
-            //adding delay between the sounds
-            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(50)) {
-                self.playNextItem()
-            }
-            
-        }else{
-            //TO DO: User turn
-            self.resetButtonsHighlights()
-        }
-    }
-    
-    func checkPressedButton(buttonPressed: Int) {
-        
-        if buttonPressed == soundsSequence[numberOfButtonsPressed] {
-            
-            if numberOfButtonsPressed == (defaultMaxLevel - 1) {
-                print("WON THE GAME")
-            } else if numberOfButtonsPressed == soundsSequence.count - 1 {
-                //at the last item of the sequence
-            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
-                //delay for the next level... add animation?
-                    self.startNewLevel()
-                }
-            }
-            numberOfButtonsPressed += 1
-            
-        } else {
-            //GAME OVER
-            print("game over!")
-            label.text = "GAME OVER!"
-            
-        }
+        print("Player finish playing", player)
+        self.game.afterSoundIsPlayed()
     }
 }
 
@@ -273,5 +165,4 @@ modeBtn.setImage(UIImage(named: "gameMode_btn"), for: .normal)
 // Present the view controller in the Live View window
 let viewController = MyViewController()
 viewController.preferredContentSize = CGSize(width: 500, height: 500)
-
 PlaygroundPage.current.liveView = viewController
--- playsound.playground/Sources/DoReMiFaGame.swift
+++ playsound.playground/Sources/DoReMiFaGame.swift
@@ -1,5 +1,135 @@
 import Foundation
+import AVFoundation
+import UIKit
 
-class DoReMiFaGame {
+public class DoReMiFaGame {
+    
+    //save the sound sequence of the game
+    var soundsSequence = [Int]()
+    
+    var numberOfButtonsPressed = 0
+    var currentItem = 0
+    public var currentLevel = 0
+    let defaultMaxLevel = 5
+    
+    var gameState = GameState.NotPlaying
+    
+    var sounds : [AVAudioPlayer]
+    
+    //colored buttons
+    let pinkBtn: UIButton
+    let yellowBtn: UIButton
+    let greenBtn: UIButton
+    let purpleBtn: UIButton
+    
+    
+    public init(sounds: [AVAudioPlayer], pinkBtn: UIButton, yellowBtn: UIButton, greenBtn: UIButton, purpleBtn: UIButton) {
+        self.sounds = sounds
+        self.pinkBtn = pinkBtn
+        self.yellowBtn = yellowBtn
+        self.greenBtn = greenBtn
+        self.purpleBtn = purpleBtn
+    }
+    
+    func addSoundToSequence() {
+        let randomNumber = Int(arc4random_uniform(4))
+        self.soundsSequence.append(randomNumber)
+    }
+    
+    public func startNewLevel() {
+        self.currentLevel += 1
+        print("New level: \(currentLevel)")
+        self.currentItem = 0
+        self.numberOfButtonsPressed = 0
+        
+        self.addSoundToSequence()
+        print("Sound Sequence:",soundsSequence)
 
+        self.playNextItem()
+    }
+    
+    public func resetGame(){
+        self.currentLevel = 0
+        self.currentItem = 0
+        self.numberOfButtonsPressed = 0
+        self.soundsSequence = []
+    }
+    
+    public func afterSoundIsPlayed(){
+        if self.currentItem <= self.soundsSequence.count - 1 {
+            //adding delay between the sounds
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(50)) {
+                self.playNextItem()
+            }
+        }else{
+            //TO DO: User turn
+            self.resetButtonsHighlights()
+        }
+    }
+    
+    public func playNextItem () {
+        print("played sound :", soundsSequence[currentItem])
+        
+        let playedSound = soundsSequence[currentItem]
+        
+        self.resetButtonsHighlights()
+        self.highlightButton(buttonTag: playedSound)
+        self.sounds[playedSound].play()
+        self.currentItem += 1
+        
+    }
+    
+    
+    func resetButtonsHighlights() {
+        self.pinkBtn.setImage(UIImage(named: "pink_btn"), for: .normal)
+        self.yellowBtn.setImage(UIImage(named: "yellow_btn"), for: .normal)
+        self.greenBtn.setImage(UIImage(named: "green_btn"), for: .normal)
+        self.purpleBtn.setImage(UIImage(named: "purple_btn"), for: .normal)
+    }
+    
+    
+    func highlightButton(buttonTag: Int) {
+        switch buttonTag {
+        case 0:
+            self.resetButtonsHighlights()
+            self.pinkBtn.setImage(UIImage(named:"pink_btn_on"), for: .normal)
+            break
+        case 1:
+            self.resetButtonsHighlights()
+            self.yellowBtn.setImage(UIImage(named:"yellow_btn_on"), for: .normal)
+            break
+        case 2:
+            self.resetButtonsHighlights()
+            self.greenBtn.setImage(UIImage(named:"green_btn_on"), for: .normal)
+            break
+        case 3:
+            self.resetButtonsHighlights()
+            self.purpleBtn.setImage(UIImage(named:"purple_btn_on"), for: .normal)
+            break
+        default:
+            break
+        }
+    }
+    
+    public func checkPressedButton(buttonPressed: Int) {
+        
+        if buttonPressed == self.soundsSequence[self.numberOfButtonsPressed] {
+            
+            if self.numberOfButtonsPressed == (self.defaultMaxLevel - 1) {
+                print("WON THE GAME")
+            } else if self.numberOfButtonsPressed == self.soundsSequence.count - 1 {
+                //at the last item of the sequence
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+                    //delay for the next level... add animation?
+                    self.startNewLevel()
+                }
+            }
+            self.numberOfButtonsPressed += 1
+            
+        } else {
+            //GAME OVER
+            print("game over!")
+            
+        }
+    }
 }
