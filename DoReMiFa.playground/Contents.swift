4eca454ebf7649acee452c691decf3ecced662de
fanny <fc2@cin.ufpe.br>
2018-03-31 15:11:32 -0300

    Personalizing markup

--- DoReMiFa.playground/Contents.swift
+++ DoReMiFa.playground/Contents.swift
@@ -1,13 +1,19 @@
-//: A UIKit based Playground for presenting user interface
+
+/*:
+ ![](doremifa_logo_small.png)
+ This playground uses UIKit and AVFoundation, and it haves two main funcionallities:
+ - **DoReMiFa Game:** A sound memory game, the user can try to memorize the sequence and play along.
+ 
+ - **DoReMiFa Recorder:** You can record a mini track using the available notes to listen later. 
+ */
 
 import UIKit
 import AVFoundation
 import PlaygroundSupport
-
-//Adding custom font to the project
+//:Adding custom font to the project
 let cfURL = Bundle.main.url(forResource: "Cardenio Modern Bold", withExtension: "ttf")! as CFURL
 CTFontManagerRegisterFontsForURL(cfURL, CTFontManagerScope.process, nil)
-
-//Present the view controller in the Live View window
+//: Setting the DoReMiFaViewController
 let myViewController = DoReMiFaViewController()
+//:Present the DoReMiFaView in the Live View window
 PlaygroundPage.current.liveView = myViewController.view
