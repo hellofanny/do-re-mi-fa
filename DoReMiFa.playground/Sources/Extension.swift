1f04f02a52f2ff5d6d056800eb07fa339ed0e4dc
Fanny <fc2@academy.cin.ufpe.br>
2019-03-17 15:19:12 -0300

    Update Extension.swift

--- DoReMiFa.playground/Sources/Extension.swift
+++ DoReMiFa.playground/Sources/Extension.swift
@@ -4,14 +4,14 @@ import UIKit
 extension UIView {
     
     public func fadeIn(_ duration: TimeInterval = 1.0, delay: TimeInterval = 0.2, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
-        UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions.curveEaseIn, animations: {
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseIn, animations: {
             self.alpha = 1.0
         }, completion: completion)
     }
     
     
     public func fadeOut(_ duration: TimeInterval = 1.0, delay: TimeInterval = 0.2, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
-        UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions.curveEaseIn, animations: {
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseIn, animations: {
             self.alpha = 0.0
         }, completion: completion)
     }
